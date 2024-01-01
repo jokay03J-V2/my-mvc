0ba81c6fae7ded95823dc9e0b5f85c8683d46962
jokay03J <magrojoan3@gmail.com>
2024-01-01 21:08:56 +0100

    load config in router

--- router.php
+++ router.php
@@ -1,6 +1,20 @@
 <?php
 use Project\Core\Router;
 
+define("ROOT", str_replace("\\", "/", __DIR__));
+
+// Loads config
+if (file_exists(ROOT . "/config.json")) {
+    $routes = json_decode(file_get_contents(ROOT . "/config.json"), true);
+    $configResult = array();
+    foreach ($routes as $key => $value) {
+        $configResult[$key] = $value;
+    }
+    define("CONFIG", $configResult);
+} else {
+    throw new Exception("You must have a config");
+}
+
 // Instanciate router
 $router = new Router();
 
