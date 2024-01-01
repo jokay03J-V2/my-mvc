b3bb8f56cac95acc7d78974e766fb120f0b57ea7
jokay03J <magrojoan3@gmail.com>
2024-01-01 21:04:46 +0100

    add public entry

--- public/index.php
+++ public/index.php
@@ -5,20 +5,7 @@ require "../vendor/autoload.php";
 date_default_timezone_set("Europe/Paris");
 // Start session
 session_start();
-// Loads config
-if (file_exists("../config.json")) {
-    $routes = json_decode(file_get_contents("../config.json"), true);
-    $configResult = array();
-    foreach ($routes as $key => $value) {
-        $configResult[$key] = $value;
-    }
-    define("CONFIG", $configResult);
-    define("ROOT", "../");
-    define("SRC", "src/");
-} else {
-    throw new Exception("You must have a config");
-}
 
 // Load router
-include(ROOT . "./router.php");
+include("../router.php");
 ?>
